951d991955652417a3258926a3124e20e20e94f2
ANSUMAN PADHI <139491282+Ansuman-007@users.noreply.github.com>
2024-02-12 14:13:41 +0530

    Add files via upload
    gg

---
+++ script.js
@@ -0,0 +1,29 @@
+// Countdown Timer
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  }
+  
+  window.onload = function () {
+    var tenMinutes = 60 * 10,
+        display = document.querySelector('#timer');
+    startTimer(tenMinutes, display);
+  };
+  
+  // Slide-in Form
+  setTimeout(function(){
+    document.getElementById('slide-in-form').classList.add('show');
+  }, 15000);
+  
